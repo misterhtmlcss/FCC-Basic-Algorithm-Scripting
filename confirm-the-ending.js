09075ecf9d6cfc94c22bdba26bf4b34d447ad1e5
Roger K <misterhtmlcss@gmail.com>
2017-01-26 18:13:22 -0700

    added some substr code suggested by my friend Mike Suchor and rounded this project out with some tests using substr instead of the layered sophistication I used before.

--- confirm-the-ending.js
+++ confirm-the-ending.js
@@ -1,50 +1,35 @@
 /*
--------------------   Confirm the Ending    -------------------
+-------------------   Project Goal Start Notes  -------------------
 
 Check if a string (first argument, str) ends with the given target string (second argument, target).
 
 This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 
-Remember to use Read-Search-Ask if you get stuck. Write your own code.
-
-Here are some helpful links:
-
-String.prototype.substr()
-String.prototype.substring()
+-------------------   Project Goal End Notes    -------------------
 */
+
 var str;
 var target;
 var confirmEnding =  function(str, target) {
     if (str.length > 0 && target.length > 0) {
         
-        // String management here. Everything gets standardized. No extra spaces, no non-alpha info and all lowercase. Stripped bare basically.
-        str = str.replace(/\W+/g, " ").trim().toLowerCase().split(" ");
+        // String conditioning for process. Everything gets standardized. No extra spaces, no non-alpha info and all lowercase. Stripped bare basically.
+        str = str.replace(/\W+/g, " ").trim().toLowerCase();
         target = target.replace(/\W+/g, " ").trim().toLowerCase();
-
-        // Wanted a new var to make it easy to figure out what stage in my code I'm at visually. A good var name helps.
-        var myEndArrTargetString; 
-        myEndArrTargetString = str[str.length-1];  //Last word in array passed to myEndArrTargetString.
         
-        // About to establish the max number of times I need to loop through target and/or myEndArrTargetString to determine match.
-        var loopSize;
-        if (myEndArrTargetString.length > target.length) {
-            loopSize = target.length; // Is target shorter? If so I want to iterate only as many times as I have letters e.g. target
-            } else {
-            loopSize = myEndArrTargetString.length; // myEndArrTargetString is shorter or equal to target, so this is the number of times I loop.
-                } 
-        var i = loopSize - 1;
-
-        for (i; i > 0; i--) {
-
-            if (myEndArrTargetString[i] == target[i]){
-                return true;
-                console.log("returning true " + myEndArrTargetString[i] + " " + myEndArrTargetString);
-            } else {
-                    console.log("Not the same and definitely not similar ---- returning false! " + myEndArrTargetString[i] + " " + myEndArrTargetString);
+        // filtering out final word and then determining if it's a match.
+        str = str.substr(-target.length);
+                if(str === target) {
+                    return true;
+                    // console.log(str + " " + target);
+                } else {
                     return false;
+                    // console.log(str + " " + target);
                     }
-            }
-        }
+            return str;
+        }  /*else {
+            console.log("Error. You need to have a string and an submitted 'target' string to use this function.")
+        }*/
 };
         
 confirmEnding("Hello World, what  are  you doing?", "worldWTF");
